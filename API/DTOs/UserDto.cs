20935495cc8531c92ae5f8f5a16128d3d9723fc5
Tarun <tarunkrai@gmail.com>
2024-08-30 11:55:32 +0530

    End of section 4

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Token { get; set; }
+    }
+}
