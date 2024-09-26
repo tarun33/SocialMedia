da0ad60eefb3784b701f67baf2262e11c65f404e
Tarun <tarunkrai@gmail.com>
2024-09-26 13:57:39 +0530

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,8 +47,10 @@ public class AccountController(DataContext context,ITokenService tokenService):B
     [HttpPost("login")]
     public async  Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x =>
-         x.UserName == loginDto.Username.ToLower()); 
+        var user = await context.Users
+        .Include(p => p.Photos)
+            .FirstOrDefaultAsync(x =>
+                x.UserName == loginDto.Username.ToLower()); 
 
          if (user == null) return Unauthorized("Invalid Usersname");
 
@@ -64,7 +66,8 @@ public class AccountController(DataContext context,ITokenService tokenService):B
          return new UserDto
          {
             Username = user.UserName,
-            Token = tokenService.CreateToken(user)
+            Token = tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x=> x.IsMain)?.Url
          };
     }
 }
--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -22,7 +23,9 @@ namespace API.Extensions
             services.AddCors();
             services.AddScoped<ITokenService,TokenService>();
             services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IPhotoService,PhotoService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
             return services;
         }
